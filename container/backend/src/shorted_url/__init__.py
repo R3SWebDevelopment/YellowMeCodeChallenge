4e259bdffd090c218dcb0caa445a338332162f1b
Ricardo Tercero Solis <ricardo.tercero@r3s.com.mx>
2021-01-24 16:00:58 -0600

    Allow CORS on backend

--- container/backend/src/shorted_url/__init__.py
+++ container/backend/src/shorted_url/__init__.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask
+from flask_cors import CORS
 from . import views
 from . import db
 
@@ -12,7 +13,7 @@ def create_app(test_config=None):
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'shorted.sqlite'),
     )
-
+    CORS(app, resources={r"/*": {"origins": "*"}})
     if test_config is None:
         # load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
